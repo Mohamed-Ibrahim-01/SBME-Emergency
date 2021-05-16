6041223dfd7ea5262e7ee65ec18fc1c3c38da551
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-05-16 17:21:02 +0200

    AddRoutes(new):[Adding emergency route]

--- emergency/blueprints/main/routes.py
+++ emergency/blueprints/main/routes.py
@@ -6,3 +6,7 @@ main = Blueprint(name='main', import_name=__name__)
 @main.route('/home')
 def home():
     return render_template('/main/home.html',data="Hello Flask")
+
+@main.route('/emergency')
+def home():
+    return render_template('/main/emergency.html',data="This is the emergency page")
