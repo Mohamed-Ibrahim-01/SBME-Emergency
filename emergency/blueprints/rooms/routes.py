6c79aa595bbf68ebaf9a919f574f8769ac67bd8a
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-06-02 04:16:59 +0200

    AddingRoutes(new):[Adding room & overview routes]

--- emergency/blueprints/rooms/routes.py
+++ emergency/blueprints/rooms/routes.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from emergency.miniorm.miniorm import miniorm as db
+import datetime
 
-emergency = Blueprint(name='emergency', import_name=__name__)
+emergency = Blueprint(name='emergency', import_name=__name__, url_prefix="/emergency")
+currDate = datetime.datetime.now()
+stopDate = datetime.datetime(currDate.year, currDate.month -3, currDate.day) 
 
-@emergency.route('/emergency')
-def home():
-    return render_template('/room/emergency.html',data="This is the emergency page")
+@emergency.route('/overview')
+def overview():
+    return render_template('/room/overview.html',data="This is the emergency page")
+
+@emergency.route('/<int:roomNum>')
+def room(roomNum):
+    db.getEmergency(roomNum, stopDate)
+    return render_template('/room/emergencyRooms.html',data="This is the emergency page")
