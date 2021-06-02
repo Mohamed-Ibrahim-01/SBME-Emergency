21fee263b8778547140c1cbeae315cee95db284e
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-06-02 04:16:38 +0200

    AddingRoutes(new):[Adding login route]

--- emergency/blueprints/main/routes.py
+++ emergency/blueprints/main/routes.py
@@ -7,3 +7,7 @@ main = Blueprint(name='main', import_name=__name__)
 def home():
     return render_template('/main/home.html',data="Hello Flask")
 
+@main.route('/login')
+def login():
+    return render_template('/main/login.html',data="Login Page")
+
