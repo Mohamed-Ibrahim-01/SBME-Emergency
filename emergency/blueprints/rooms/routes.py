03205d68b2c6a47f5321094a435e92e327598a99
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-06-05 15:34:04 +0200

    adding 'addPatient' GET handler

--- emergency/blueprints/rooms/routes.py
+++ emergency/blueprints/rooms/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
+from emergency.forms.patients import PatientForm
 from emergency.miniorm.miniorm import miniorm as db
 import datetime
 
 emergency = Blueprint(name='emergency', import_name=__name__, url_prefix="/emergency")
 currDate = datetime.datetime.now()
-stopDate = datetime.datetime(currDate.year, currDate.month -3, currDate.day) 
+stopDate = datetime.datetime(currDate.year, currDate.month -3, currDate.day)
 
 @emergency.route('/overview')
 def overview():
@@ -14,3 +15,8 @@ def overview():
 def room(roomNum):
     db.getEmergency(roomNum, stopDate)
     return render_template('/room/emergencyRooms.html',data="This is the emergency page")
+
+@emergency.route('/addPatient')
+def addPatient():
+    form = PatientForm()
+    return render_template('/patient/addPatient.html',form=form)
