2a5842e7ff207dd1260db57f07435b78da5d79e0
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-06-10 14:27:02 +0200

    Adding contactus route

--- emergency/blueprints/main/routes.py
+++ emergency/blueprints/main/routes.py
@@ -1,13 +1,30 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, url_for, redirect
+from emergency.forms.contactUs import ContactUsForm
+from emergency.miniorm.miniorm import miniorm
+from emergency.miniorm.models import *
 main = Blueprint(name='main', import_name=__name__)
+db = miniorm()
 
 @main.route('/')
 @main.route('/home')
 def home():
     return render_template('/main/home.html',data="Hello Flask")
 
-@main.route('/login')
-def login():
-    return render_template('/main/login.html',data="Login Page")
+@main.route('/contactUs',methods=['GET','POST'])
+def contactUs():
+    form = ContactUsForm()
+    if form.validate_on_submit():
+        newMessage= ContactMessage(
+            Name = form.Name.data if form.Name.data else "Anonymous",
+            Subject = form.Subject.data,
+            Email = form.Mail.data if form.Name.data else "Anonymous",
+            Message = form.Message.data
+        )
+        db.addContactMessage(newMessage)
+        return redirect(url_for('main.contactUsResponses'))
+    return render_template('/main/contactUs.html',form=form)
 
+@main.route('/contactUs/responses')
+def contactUsResponses():
+    messages = db.getAllMessages()
+    return render_template('/main/contactUsResponses.html',messages=messages)
