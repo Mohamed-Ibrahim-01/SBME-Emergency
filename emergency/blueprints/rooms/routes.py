362e27ccfe5564dac0183f47e7f35f4fbfa3c7f6
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-06-10 14:27:42 +0200

    Adding the real data from the db

--- emergency/blueprints/rooms/routes.py
+++ emergency/blueprints/rooms/routes.py
@@ -1,25 +1,61 @@
-from flask import Blueprint, render_template, url_for, redirect, flash
+from flask import Blueprint, render_template, url_for, redirect
 from emergency.forms.patients import PatientForm
-from emergency.miniorm.miniorm import miniorm as db
+from emergency.forms.doctors import DoctorForm
+from emergency.miniorm.miniorm import miniorm
+from emergency.miniorm.models import *
 import datetime
 
 emergency = Blueprint(name='emergency', import_name=__name__, url_prefix="/emergency")
 currDate = datetime.datetime.now()
 stopDate = datetime.datetime(currDate.year, currDate.month -3, currDate.day)
+db = miniorm()
 
 @emergency.route('/overview')
 def overview():
-    return render_template('/room/overview.html',data="This is the emergency page")
+    doctors = db.getAllDoctors()
+    patients = db.getAllPatients()
+    return render_template('/room/overview.html',data={ "doctors": doctors, "patients": patients})
 
 @emergency.route('/<int:roomNum>')
 def room(roomNum):
-    db.getEmergency(roomNum, stopDate)
     return render_template('/room/emergencyRooms.html',data="This is the emergency page")
 
 @emergency.route('/addPatient', methods=['GET','POST'])
 def addPatient():
     form = PatientForm()
     if form.validate_on_submit():
-        print("\n\n",form.Name.data,form.Address.data,form.Case.data,"\n\n")
-        return redirect(url_for('main.home'))
+      newPatient= Patient(
+        Email=form.Mail.data,
+        PatientSsn=form.Ssn.data,
+        Name=form.Name.data,
+        Phone=form.Phone.data,
+        Sex=form.Sex.data,
+        BirthDate=form.BirthDate.data,
+        PatientCase=form.Case.data,
+        RoomNum="1",
+        Address=form.Address.data
+      )
+      db.addPatient(newPatient)
+      return redirect(url_for('emergency.overview'))
     return render_template('/patient/addPatient.html',form=form)
+
+@emergency.route('/addDoctor', methods=['GET','POST'])
+def addDoctor():
+    form = DoctorForm()
+    print (form.validate_on_submit())
+    if form.validate_on_submit():
+        newDoctor= Doctor(
+          Email=form.Mail.data,
+          DoctorSsn=form.Ssn.data,
+          Name=form.Name.data,
+          Phone=form.Phone.data,
+          Sex=form.Sex.data,
+          BirthDate=form.BirthDate.data,
+          MainQualification=form.MainQualification.data,
+          ExtraQualification=form.ExtraQualification.data,
+          RoomNum="1",
+          Address=form.Address.data
+        )
+        db.addDoctor(newDoctor)
+        return redirect(url_for('emergency.overview'))
+    return render_template('/doctor/addDoctor.html',form=form)
